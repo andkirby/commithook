247149c32daee7dbf208aa05369381a67514c15d
Andrew Roslik <andkirby@gmail.com>
2014-07-03 18:05:56 +0300

    - Updated config model. -- Added method to get assoc array from config. -- Separated method getNode() to separate logic getting array and string.

--- LibHooks/lib/PreCommit/Config.php
+++ LibHooks/lib/PreCommit/Config.php
@@ -6,6 +6,8 @@ namespace PreCommit;
  */
 class Config extends \SimpleXMLElement
 {
+    const XPATH_START = '/config/';
+
     /**
      * Self instance
      *
@@ -18,10 +20,14 @@ class Config extends \SimpleXMLElement
      *
      * @param array $options
      * @return Config
+     * @throws Exception
      */
     static public function getInstance(array $options = array())
     {
         if (!self::$_instance) {
+            if (!isset($options['file'])) {
+                throw new Exception('Options parameter "file" is required.');
+            }
             if (!file_exists($options['file'])) {
                 $options['file'] = $options['file'] . '.dist';
             }
@@ -34,18 +40,47 @@ class Config extends \SimpleXMLElement
      * Get node by xpath
      *
      * @param string $xpath
-     * @param bool $isArray
      * @return array|Config
      */
-    public function getNode($xpath, $isArray = false)
+    public function getNode($xpath)
     {
-        $result = self::$_instance->xpath('/config/' . $xpath);
-        if ($isArray) {
-            return $result;
-        } else {
-            return isset($result[0]) ? $result[0] : null;
-        }
+        $result = $this->xpath(self::XPATH_START . $xpath);
+        $result = isset($result[0]) ? (string)$result[0] : null;
+        return $result;
+    }
 
+    /**
+     * Get node array values
+     *
+     * @param string $xpath
+     * @return array|null
+     */
+    public function getNodeArray($xpath)
+    {
+        $result = $this->xpath(self::XPATH_START . $xpath);
+        $result = isset($result[0]) ? (array)$result[0] : null;
+        return $result;
+    }
+
+    /**
+     * Get node multi values by xpath
+     *
+     * Multi-values means nodes with the same name in the same place
+     *
+     * @param string $xpath
+     * @return array|Config
+     */
+    public function getMultiNode($xpath)
+    {
+        $last = null;
+        $arr = explode('/', $xpath);
+        $last = array_pop($arr);
+        $xpath = implode('/', $arr);
+
+        $result = $this->getNodeArray($xpath);
+
+        $result = isset($result[$last]) ? $result[$last] : null;
+        return $result;
     }
 }
 
