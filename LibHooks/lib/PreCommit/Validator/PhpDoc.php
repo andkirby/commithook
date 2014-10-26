3be0446c78b6ecb50f0af547d8a2637162f7eb65
Andrew Roslik <andkirby@gmail.com>
2014-10-26 19:26:08 +0200

    - Added validator to check VCS conflicts in files.

--- LibHooks/lib/PreCommit/Validator/PhpDoc.php
+++ LibHooks/lib/PreCommit/Validator/PhpDoc.php
@@ -215,9 +215,6 @@ class PhpDoc extends AbstractValidator
         )) {
             $this->_addError($file, self::CODE_PHP_DOC_VAR_EMPTY, count($matches[0]));
         }
-        print_r($file . PHP_EOL);
-        print_r($matches);
-        die;
         return $this;
     }
 
