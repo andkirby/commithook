2bea42ad6c8d69e331ebe712d60b300fe652860c
Andrew Roslik <andkirby@gmail.com>
2015-01-19 01:46:13 +0200

    - Fixed namespace exception usage.

--- LibHooks/lib/PreCommit/Composer/Command/Install.php
+++ LibHooks/lib/PreCommit/Composer/Command/Install.php
@@ -2,6 +2,7 @@
 namespace PreCommit\Composer\Command;
 
 use Composer\Command\Helper\DialogHelper;
+use PreCommit\Composer\Exception;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
