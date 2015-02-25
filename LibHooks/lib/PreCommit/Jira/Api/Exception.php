a7d5df6acdf8dad912775ad4b819660b6d2604ae
Andrew Roslik <andkirby@gmail.com>
2015-02-26 01:23:57 +0200

    - Added Jira lib extra files.

---
+++ LibHooks/lib/PreCommit/Jira/Api/Exception.php
@@ -0,0 +1,13 @@
+<?php
+namespace PreCommit\Jira\Api;
+
+use chobie\Jira as JiraLib;
+
+/**
+ * Class JIRA API Exception
+ *
+ * @package PreCommit\Jira\Api
+ */
+class Exception extends JiraLib\Api\Exception
+{
+}
---
+++ LibHooks/lib/PreCommit/Jira/_password.lib.php
@@ -0,0 +1,36 @@
+<?php
+namespace ILBYNINKHULN;
+
+/**
+ * Class Password
+ *
+ * @package Jigit\Jira
+ */
+class Password
+{
+    /**
+     * Constructor
+     *
+     * @param string $file
+     */
+    public function __construct($file)
+    {
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     * @throws PasswordException
+     */
+    public function getPassword()
+    {
+    }
+}
+
+/**
+ * Class PasswordException
+ */
+class PasswordException extends \Exception
+{
+}
