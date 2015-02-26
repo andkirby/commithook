0e9d0fc39aa17bc74024cba7a0db184c93374ceb
Andrew Roslik <andkirby@gmail.com>
2015-02-26 02:51:12 +0200

    - Pushed runner.php to vendor/autoload.php usage.

--- LibHooks/runner.php
+++ LibHooks/runner.php
@@ -17,8 +17,15 @@ set_include_path(
     )
 );
 
-require_once 'lib/PreCommit/Autoloader.php';
-\PreCommit\Autoloader::register();
+if (is_file(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+} else {
+    require_once 'autoload.php';
+}
+
+//Deprecated autoloader since v1.6.10
+//require_once 'lib/PreCommit/Autoloader.php';
+//\PreCommit\Autoloader::register();
 
 set_error_handler('\PreCommit\ErrorHandler::handleError');
 
