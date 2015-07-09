13afa0c9a4dfe4045182b953694996e4dc165d64
Andrew Roslik <andkirby@gmail.com>
2015-07-09 20:06:25 +0300

    - Added removing user XML comments.

--- LibHooks/lib/PreCommit/Config.php
+++ LibHooks/lib/PreCommit/Config.php
@@ -245,6 +245,9 @@ class Config extends \SimpleXMLElement
         $result = $this->xpath(self::XPATH_START . $xpath);
         $result = isset($result[0]) ? (array)$result[0] : array();
         $result = json_decode(json_encode($result), true);
+
+        //remove XML comments (hack) TODO investigate a problem
+        unset($result['comment']);
         return $result;
     }
 
