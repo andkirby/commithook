dba5b27548085fa8a886a46bc389413851017ab2
Andrew Roslik <andkirby@gmail.com>
2015-07-30 00:43:07 +0300

    - Updated cache namespace name.

--- LibHooks/lib/PreCommit/Issue/JiraAdapter.php
+++ LibHooks/lib/PreCommit/Issue/JiraAdapter.php
@@ -146,7 +146,7 @@ class JiraAdapter extends AdapterAbstract implements AdapterInterface
             array(
                 'cache_dir' => $this->_getCacheDir(),
                 'ttl'       => 7200,
-                'namespace' => 'issue-' . self::CACHE_SCHEMA_VERSION
+                'namespace' => 'issue-jira-' . self::CACHE_SCHEMA_VERSION
             )
         );
     }
