474029a4a6caef5f66a751108ab459b5ed3cb4e7
Andrew Roslik <andkirby@gmail.com>
2015-10-11 22:31:00 +0300

    #20: Check if @return tag returns $this on returning parent method which returns $this.  - Added using class aliases.

--- LibHooks/lib/PreCommit/Validator/ParentThis.php
+++ LibHooks/lib/PreCommit/Validator/ParentThis.php
@@ -37,9 +37,12 @@ class ParentThis extends AbstractValidator
         if (!$parentClass) {
             return false;
         }
-
         $this->_validateParentClassInReturn($parentClass, $content, $file);
 
+        $parentClassAlias = $this->_getClassAlias($parentClass, $content);
+        if ($parentClassAlias) {
+            $this->_validateParentClassInReturn($parentClassAlias, $content, $file);
+        }
         return !$this->_errorCollector->hasErrors();
     }
 
@@ -57,6 +60,44 @@ class ParentThis extends AbstractValidator
     }
 
     /**
+     * Get parent class name
+     *
+     * @param string $class
+     * @param string $content
+     * @return null|string
+     */
+    protected function _getClassAlias($class, $content)
+    {
+        $matches = array();
+        if (strpos($class, '\\')) {
+            if (0 === strpos($class, '\\')) {
+                //ignore absolute class path
+                return null;
+            }
+            $parentPath = substr($class, 0, strpos($class, "\x5C"));
+            if (
+                preg_match('~use ([A-z0-9\x5C_]+)\x5C' . $parentPath . ';~', $content, $matches)
+                || preg_match('~use ([A-z0-9\x5C_]+) as ' . $parentPath . ';~', $content, $matches)
+            ) {
+                return $matches[1] . substr($class, strpos($class, "\x5C"));
+            }
+        }
+        preg_match("~use $class as ([A-z0-9_]+);~", $content, $matches);
+        if (!empty($matches[1])) {
+            return $matches[1];
+        }
+        preg_match('~use ([A-z0-9\\_]+\\' . $class . ');~', $content, $matches);
+        if (!empty($matches[1])) {
+            return $matches[1];
+        }
+        preg_match('~use ([A-z0-9\\_]+) as ' . $class . ';~', $content, $matches);
+        if (!empty($matches[1])) {
+            return $matches[1];
+        }
+        return null;
+    }
+
+    /**
      * Check if parent class matches with
      *
      * @param string $parentClass
@@ -66,7 +107,9 @@ class ParentThis extends AbstractValidator
      */
     protected function _validateParentClassInReturn($parentClass, $content, $file)
     {
-        if (preg_match('/@return[ ]+\\\?' . ltrim($parentClass, '\\') . '/', $content)) {
+        $regularClass = ltrim($parentClass, '\\'); //remove left "\"
+        $regularClass = str_replace('\\', '\x5C', $regularClass); //set codes instead "\"
+        if (preg_match('~ +\* @return +\x5C?' . $regularClass . '~', $content)) {
             $this->_addError($file, self::CODE_PHP_RETURN_NOT_THIS, $parentClass, null);
         }
         return $this;
