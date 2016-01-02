ac126d12e604d9d27ef562f4633c843f0452cd9c
Andrew Roslik <andkirby@gmail.com>
2016-01-02 20:23:41 +0100

    Implemented #43: Add checking license in files  - Added license block validator.

---
+++ LibHooks/lib/PreCommit/Validator/License.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * @license https://raw.githubusercontent.com/andkirby/commithook/master/LICENSE.md
+ */
+
+namespace PreCommit\Validator;
+
+use PreCommit\Config;
+use PreCommit\Helper\PathMatch;
+
+/**
+ * Class to validate license block in files
+ *
+ * @package PreCommit\Validator
+ */
+class License extends AbstractValidator
+{
+    /**#@+
+     * Error codes
+     */
+    const CODE_MISSED_LICENSE = 'missedLicenseBlock';
+    /**#@-*/
+
+    /**
+     * Error messages
+     *
+     * @var array
+     */
+    protected $errorMessages
+        = array(
+            self::CODE_MISSED_LICENSE => 'Missed license block.',
+        );
+
+    /**
+     * Validate content
+     *
+     * @param string $content
+     * @param string $file
+     * @return bool
+     */
+    public function validate($content, $file)
+    {
+        if (!$this->isLicenseRequired($file)) {
+            return true;
+        }
+
+        if (false === strpos($content, $this->getTestLicense())) {
+            $this->addError($file, self::CODE_MISSED_LICENSE);
+        }
+
+        return !$this->errorCollector->hasErrors();
+    }
+
+    /**
+     * Check
+     *
+     * @param string $file
+     * @return bool
+     */
+    protected function isLicenseRequired($file)
+    {
+        $matcher = new PathMatch();
+
+        return $matcher
+            ->setAllowed(
+                $this->getPaths('required')
+            )
+            ->setProtected(
+                $this->getPaths('ignored')
+            )
+            ->test($file);
+    }
+
+    /**
+     * Get license text block
+     *
+     * @return null|string
+     */
+    public function getTestLicense()
+    {
+        $test = trim(
+            $this->getConfig()->getNode(
+                'validators/License/licenses/'.$this->getLicenseName().'/test_text'
+            )
+        );
+
+        return $test ?: $this->getLicense();
+    }
+
+    /**
+     * Get paths
+     *
+     * @param string $type
+     * @return array
+     */
+    protected function getPaths($type)
+    {
+        $name = $this->getLicenseName();
+
+        $paths = array_values($this->getConfig()->getNodeArray('/validators/License/licenses/'.$name.'/paths/'.$type));
+        foreach ($paths as &$path) {
+            $path = rtrim($path, '/').'/';
+        }
+
+        $filePaths = array_values(
+            $this->getConfig()->getNodeArray('/validators/License/licenses/'.$name.'/filepaths/'.$type)
+        );
+
+        return array_merge($paths, $filePaths);
+    }
+
+    /**
+     * Get config model
+     *
+     * @return Config
+     */
+    protected function getConfig()
+    {
+        return Config::getInstance();
+    }
+
+    /**
+     * Get license name
+     *
+     * @return null|string
+     */
+    protected function getLicenseName()
+    {
+        return $this->getConfig()->getNode('license/name');
+    }
+
+    /**
+     * Get license text block
+     *
+     * @return null|string
+     */
+    public function getLicense()
+    {
+        return trim(
+            $this->getConfig()->getNode(
+                'validators/License/licenses/'.$this->getLicenseName().'/text'
+            )
+        );
+    }
+}
