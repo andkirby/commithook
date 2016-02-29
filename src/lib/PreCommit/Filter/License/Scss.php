d05ab52875a68a3af9f84f7d2c355d5b393abca3
Andrew Roslik <andkirby@gmail.com>
2016-02-29 14:34:41 +0200

    Implemented #43: Add checking license in files  - Added SCSS.

---
+++ src/lib/PreCommit/Filter/License/Scss.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @license https://raw.githubusercontent.com/andkirby/commithook/master/LICENSE.md
+ */
+namespace PreCommit\Filter\License;
+
+/**
+ * Class Scss
+ * This filter responsible for adding license block into SCSS files
+ *
+ * @package PreCommit\Filter\License
+ */
+class Scss extends Css
+{
+}
