0bcca048ade04e31f68bc8cab052d73d0513817b
Andrew Roslik <andkirby@gmail.com>
2016-02-29 10:29:15 +0200

    Implemented #60: Add an ability to validate custom path of files for `test` command  - Extended test command to set custom files for testing.

--- src/lib/PreCommit/Command/Command/Test.php
+++ src/lib/PreCommit/Command/Command/Test.php
@@ -5,8 +5,12 @@
 namespace PreCommit\Command\Command;
 
 use PreCommit\Command\Exception;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 /**
  * CommitHooks command tester
@@ -36,6 +40,24 @@ class Test extends AbstractCommand
     }
 
     /**
+     * Init input definitions
+     *
+     * @return $this
+     */
+    protected function configureInput()
+    {
+        parent::configureInput();
+
+        $this->addArgument(
+            'path',
+            InputArgument::OPTIONAL,
+            'Path to file/directory which should be tested.'
+        );
+
+        return $this;
+    }
+
+    /**
      * Execute command
      *
      * @param InputInterface  $input
@@ -47,10 +69,57 @@ class Test extends AbstractCommand
     {
         parent::execute($input, $output);
         !defined('TEST_MODE') && define('TEST_MODE', true);
+
+        $customFiles = $this->getCustomTestFiles($input, $output);
+
+        /**
+         * Define $vcsFiles for runner.php
+         *
+         * @see src/runner.php:87
+         * @var array|null $vcsFiles
+         */
+        /** @noinspection PhpUnusedLocalVariableInspection */
+        $vcsFiles = $customFiles ?: null;
+
         /** @noinspection PhpUnusedLocalVariableInspection */
         $hookFile = $this->askProjectDir($input, $output).'/.git/hooks/pre-commit';
         require_once __DIR__.'/../../../../runner.php';
 
         return 0;
+    }
+
+    /**
+     * Get custom files list for validation
+     *
+     * @param InputInterface                                    $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @return array
+     * @throws \PreCommit\Command\Exception
+     */
+    protected function getCustomTestFiles(InputInterface $input, OutputInterface $output)
+    {
+        if (!$input->getArgument('path')) {
+            //ignore an empty path
+            return null;
+        }
+
+        $customFiles = array();
+        $paths       = explode(' ', $input->getArgument('path'));
+        foreach ($paths as $path) {
+            $fs = new Filesystem();
+            if ($fs->isAbsolutePath($path)) {
+                throw new Exception('Sorry, absolute path is not supported so far.');
+            }
+
+            $finder = new Finder();
+            $finder->files()->in($path);
+
+            /** @var SplFileInfo $file */
+            foreach ($finder as $file) {
+                $customFiles[] = rtrim($path, '\\/').DIRECTORY_SEPARATOR.$file->getRelativePathname();
+            }
+        }
+
+        return $customFiles;
     }
 }
