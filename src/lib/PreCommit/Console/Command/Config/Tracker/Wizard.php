c3f1584993ff82f66721d3133043d13d586fae0d
Andrew Roslik <andkirby@gmail.com>
2016-08-15 11:27:45 +0300

    Implemented #89: Move 'commithook config wizard' to 'commithook tracker:wizard'  - Moved wizard method into Wizard.php command.

--- src/lib/PreCommit/Console/Command/Config/Tracker/Wizard.php
+++ src/lib/PreCommit/Console/Command/Config/Tracker/Wizard.php
@@ -43,6 +43,79 @@ class Wizard extends Set
     }
 
     /**
+     * Connection tracker wizard
+     *
+     * @return $this
+     */
+    protected function connectionWizard()
+    {
+        $this->output->writeln('Set up issue tracker connection.');
+
+        //type
+        $options = $this->getXpathOptions(Set::XPATH_TRACKER_TYPE);
+        $this->trackerType = $this->io->askQuestion(
+            $this->getSimpleQuestion()->getQuestion(
+                'Tracker type',
+                array_search(
+                    $this->getConfig()->getNode(Set::XPATH_TRACKER_TYPE),
+                    $options
+                ),
+                $options
+            )
+        );
+
+        //URL
+        $url = $this->io->askQuestion(
+            $this->getSimpleQuestion()->getQuestion(
+                "'{$this->trackerType}' URL",
+                $this->getConfig()->getNode($this->getXpath('url'))
+            )
+        );
+
+        //username
+        $username = $this->io->askQuestion(
+            $this->getSimpleQuestion()->getQuestion(
+                "'{$this->trackerType}' username",
+                $this->getConfig()->getNode($this->getXpath('username'))
+            )
+        );
+
+        //password
+        $question = $this->getSimpleQuestion()->getQuestion(
+            "'{$this->trackerType}' password",
+            $this->getConfig()->getNode($this->getXpath('password'))
+                ? '*****' : null
+        );
+        $question->setHiddenFallback(false);
+        $question->setHiddenFallback(true);
+        $password = $this->io->askQuestion($question);
+        $password = '*****' !== $password ? $password : null;
+
+        //project key
+        $prjKey = $this->io->askQuestion(
+            $this->getSimpleQuestion()->getQuestion(
+                "Current '{$this->trackerType}' project key",
+                $this->getConfig()->getNode($this->getXpath('project'))
+            )
+        );
+
+        $scope = $this->getScope(Set::XPATH_TRACKER_TYPE);
+
+        $scopeCredentials = Set::OPTION_SCOPE_PROJECT == $scope
+            ? $this->getCredentialsScope() : $scope;
+
+        $this->writeConfig(Set::XPATH_TRACKER_TYPE, $scope, $this->trackerType);
+        $this->writeConfig($this->getXpath('url'), $scope, $url);
+        $this->writeConfig($this->getXpath('username'), $scopeCredentials, $username);
+        if (null !== $password) {
+            $this->writeConfig($this->getXpath('password'), $scopeCredentials, $password);
+        }
+        $this->writeConfig($this->getXpath('project'), Set::OPTION_SCOPE_PROJECT, $prjKey);
+
+        return $this;
+    }
+
+    /**
      * {@inheritdoc}
      */
     protected function getScopeOption()
