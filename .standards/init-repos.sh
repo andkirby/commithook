4ed44ba9692080903a50a3347034d09204960e39
Andrew Roslik <andkirby@gmail.com>
2016-09-05 14:31:03 +0300

    Implemented #114: Add loading extra configuration package  - Added init files.

---
+++ .standards/init-repos.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+[ ! -d ./magento-ecg/.git ] && git clone git@github.com:cmhook/phpcs-magento-ecg-project.git ./magento-ecg
+[ ! -d ./symfony/.git ] && git clone git@github.com:cmhook/phpcs-symfony.git ./symfony
