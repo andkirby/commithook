259c83ec12a0b11a3be7a691c96436478766ffe3
Andrew Roslik <andkirby@gmail.com>
2016-09-06 11:12:44 +0300

    @@through Fixes for tests.

--- src/tests/testsuite/PreCommit/Test/Validator/XmlParserTest.php
+++ src/tests/testsuite/PreCommit/Test/Validator/XmlParserTest.php
@@ -3,10 +3,12 @@
  * @license https://raw.githubusercontent.com/andkirby/commithook/master/LICENSE.md
  */
 namespace PreCommit\Test\Validator;
+
+use PreCommit\Config;
 use PreCommit\Processor;
 use PreCommit\Processor\PreCommit;
-use PreCommit\Config;
 use PreCommit\Validator\XmlParser;
+use PreCommit\Vcs\Git;
 
 /**
  * Class test for Processor
@@ -18,80 +20,31 @@ class XmlParserTest extends \PHPUnit_Framework_TestCase
      *
      * @var string
      */
-    static protected $_fileTest = 'tests/testsuite/PreCommit/Test/_fixture/empty.xml';
+    static protected $fileTest = 'tests/testsuite/PreCommit/Test/_fixture/empty.xml';
 
     /**
      * Test model
      *
      * @var \PreCommit\Processor\PreCommit
      */
-    static protected $_model;
+    static protected $model;
 
     /**
      * Set up test model
      */
-    static public function setUpBeforeClass()
+    public static function setUpBeforeClass()
     {
         //init config object
-        Config::initInstance(array('file' => PROJECT_ROOT . '/commithook.xml'));
+        Config::initInstance(['file' => PROJECT_ROOT.'/config/root.xml']);
         Config::setSrcRootDir(PROJECT_ROOT);
-        $vcsAdapter = self::_getVcsAdapterMock();
+        $vcsAdapter = self::getVcsAdapterMock();
 
         /** @var PreCommit $processor */
         $processor = Processor::factory('pre-commit', $vcsAdapter);
         $processor->setCodePath(PROJECT_ROOT)
-            ->setFiles(array(self::$_fileTest));
+            ->setFiles([self::$fileTest]);
         $processor->process();
-        self::$_model = $processor;
-    }
-
-    /**
-     * Get VCS adapter mock
-     *
-     * @return object
-     */
-    protected static function _getVcsAdapterMock()
-    {
-        $generator = new \PHPUnit_Framework_MockObject_Generator();
-        $vcsAdapter = $generator->getMock('PreCommit\Vcs\Git');
-        $vcsAdapter->expects(self::once())
-            ->method('getAffectedFiles')
-            ->will(self::returnValue(array()));
-        return $vcsAdapter;
-    }
-
-    /**
-     * Get specific errors list
-     *
-     * @param string $file
-     * @param string $code
-     * @param bool $returnLines
-     * @return array
-     * @throws \PHPUnit_Framework_Exception
-     */
-    protected function getSpecificErrorsList($file, $code, $returnLines = false)
-    {
-        $errors = self::$_model->getErrors();
-        if (!isset($errors[$file])) {
-            throw new \PHPUnit_Framework_Exception('Errors for file ' . self::$_fileTest . ' not found.');
-        }
-        $errors = $errors[$file];
-
-        $this->assertArrayHasKey($code, $errors);
-        if (!isset($errors[$code])) {
-            throw new \PHPUnit_Framework_Exception("Errors for code $code not found.");
-        }
-
-        $list = array();
-        $key = $returnLines ? 'line' : 'value';
-        foreach ($errors[$code] as $item) {
-            if ($key == 'value' && isset($item['line'])) {
-                $list[$item['line']] = $item[$key];
-            } else {
-                $list[] = $item[$key];
-            }
-        }
-        return $list;
+        self::$model = $processor;
     }
 
     /**
@@ -100,13 +53,59 @@ class XmlParserTest extends \PHPUnit_Framework_TestCase
     public function testEmptyFile()
     {
         $errors = $this->getSpecificErrorsList(
-            self::$_fileTest,
+            self::$fileTest,
             XmlParser::CODE_XML_ERROR
         );
 
-        $expected = array (
-            'DOMDocument::loadXML(): Empty string supplied as input',
-        );
-        $this->assertEquals($expected, array_values($errors));
+        $expected = 'Empty string supplied as input';
+        $this->assertContains($expected, array_shift($errors));
+    }
+
+    /**
+     * Get VCS adapter mock
+     *
+     * @return object
+     */
+    protected static function getVcsAdapterMock()
+    {
+        $vcsAdapter = new Git();
+        $vcsAdapter->setAffectedFiles([]);
+
+        return $vcsAdapter;
+    }
+
+    /**
+     * Get specific errors list
+     *
+     * @param string $file
+     * @param string $code
+     * @param bool   $returnLines
+     * @return array
+     * @throws \PHPUnit_Framework_Exception
+     */
+    protected function getSpecificErrorsList($file, $code, $returnLines = false)
+    {
+        $errors = self::$model->getErrors();
+        if (!isset($errors[$file])) {
+            throw new \PHPUnit_Framework_Exception('Errors for file '.self::$fileTest.' not found.');
+        }
+        $errors = $errors[$file];
+
+        $this->assertArrayHasKey($code, $errors);
+        if (!isset($errors[$code])) {
+            throw new \PHPUnit_Framework_Exception("Errors for code $code not found.");
+        }
+
+        $list = [];
+        $key  = $returnLines ? 'line' : 'value';
+        foreach ($errors[$code] as $item) {
+            if ($key == 'value' && isset($item['line'])) {
+                $list[$item['line']] = $item[$key];
+            } else {
+                $list[] = $item[$key];
+            }
+        }
+
+        return $list;
     }
 }
