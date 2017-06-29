6be28afe082748671023ab142c1ff392744906dd
Andrew Roslik <andkirby@gmail.com>
2017-06-29 16:50:37 +0300

    @@through Added error function.

--- src/shell/update-version.sh
+++ src/shell/update-version.sh
@@ -12,23 +12,33 @@ set -o xtrace
 
 VERSION_DRY_RUN=0
 
+ERR_NO_INVALID_ARG=3
+ERR_NO_LOGICAL=4
+ERR_NO_USER_EXIT=6
+
 # Set magic variables for current file & dir
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 readonly __dir __file
+
+check_error () {
+  local status=${1}
+  shift
+  if [ '0' != "${status}" ]; then
+    echo "error: $@" > /dev/stderr
+    exit ${status}
+  fi
+}
 
 cd "${__dir}/.."
 
 current_branch=$(git branch | grep '*' | grep -Eo '[^* ]+')
 echo -n "Use '${current_branch}' branch? (y/n) "
 read answer
-if [ -z $(echo "${answer}" | grep -i "^y") ]; then
-    # Answer is not YES
-    echo Stop!
-    exit 1
+if [ -z "$(echo "${answer}" | grep -i "^y")" ]; then
+  # Answer is not YES
+  check_error ${ERR_NO_USER_EXIT} 'Stop!'
 fi
-
-if [ $? != 0 ]; then echo "error: Can't go to master branch."; exit 1; fi
 
 # reset files with the version
 git checkout -- config/root.xml && git checkout -- lib/PreCommit/Command/Application.php
@@ -41,25 +51,22 @@ echo "Current version: ${current_version}"
 
 match=$(grep " = '${current_version}'" ${__dir}/../lib/PreCommit/Command/Application.php)
 if [ -z "${match}" ]; then
-    echo "error: The same version '${current_version}' not found in PreCommit/Command/Application.php file."
-    exit 1
+  check_error ${ERR_NO_LOGICAL} "error: The same version '${current_version}' not found in PreCommit/Command/Application.php file."
 fi
 
-last_tag_name=$(git tag -l | sort -V | tail -1)
+last_tag_name=$(git tags | tail -1)
 last_version=$(echo "${last_version}" | sed 's/^v\(.*\)/\1/')
 
 # Ask about selected version
 echo -n "Is it correct version to change? ${last_version} (y/n) "
 read answer
 if [ -z $(echo "${answer}" | grep -i "^y") ]; then
-    # Answer is not YES
-    echo Stop!
-    exit 1
+  # Answer is not YES
+  check_error ${ERR_NO_USER_EXIT} 'Stop!'
 fi
 
 echo ${current_version} > ${__dir}/../../dev_version \
-    && echo ${last_version} > ${__dir}/../../release_version
-if [ $? != 0 ]; then echo "error: Can't create version files."; exit 1; fi
+  && echo ${last_version} > ${__dir}/../../release_version
 
 # replace version
 sed 's|<version>'"${current_version}"'</version>|<version>'"${last_version}"'</version>|g' ${__dir}/../config/root.xml \
@@ -68,7 +75,6 @@ sed 's|<version>'"${current_version}"'</version>|<version>'"${last_version}"'</v
 && sed "s| = '${current_version}';| = '${last_version}';|g" ${__dir}/../lib/PreCommit/Command/Application.php \
     > ${__dir}/../lib/PreCommit/Command/Application.php.tmp \
     && mv ${__dir}/../lib/PreCommit/Command/Application.php.tmp ${__dir}/../lib/PreCommit/Command/Application.php
-if [ $? != 0 ]; then echo "error: Can't update version in files."; exit 1; fi
 
 # commit updated files
 git add ${__dir}/../../src/lib/PreCommit/Command/Application.php && \
@@ -81,14 +87,12 @@ git status
 echo -n "Correct files for commit? (y/n) "
 read answer
 if [ -z $(echo "${answer}" | grep -i "^y") ]; then
-    # Answer is not YES
-    echo Stop!
-    exit 1
+  # Answer is not YES
+  check_error ${ERR_NO_USER_EXIT} 'Stop!'
 fi
 
 # git tag-move -- custom command
 # Make commit, add tag, revert this commit
 git commit -m "@@through Update version to ${last_tag_name}." \
-    && git tag-move ${last_tag_name} \
-    && git revert HEAD
-
+  && git tag ${last_tag_name} \
+  && git revert HEAD
