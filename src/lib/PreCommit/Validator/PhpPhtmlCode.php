e854add7c46da2127fa49718e7f0716af076c449
Andrew Roslik <andkirby@gmail.com>
2017-06-30 12:57:49 +0300

    Implemented #132: Add PHP code validation for PHTML files  - Added validator PhpPhtmlCode for PHP code validation in PHTML files.

---
+++ src/lib/PreCommit/Validator/PhpPhtmlCode.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @license https://raw.githubusercontent.com/andkirby/commithook/master/LICENSE.md
+ */
+
+namespace PreCommit\Validator;
+
+/**
+ * Class validator for checking PHP errors in PHTML files
+ *
+ * @package PreCommit\Validator
+ */
+class PhpPhtmlCode extends PhpClass
+{
+}
