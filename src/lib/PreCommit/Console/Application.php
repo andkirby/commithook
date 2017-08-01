353fee75628137f512a88361d78952daea8b3122
Andrew Roslik <andkirby@gmail.com>
2017-08-01 14:15:18 +0300

    Revert "@@through Update version to v2.0.0-beta.44."
    This reverts commit 6aae99ddebe30e551ff89d57cb114bb3fbf26609.

--- src/lib/PreCommit/Console/Application.php
+++ src/lib/PreCommit/Console/Application.php
@@ -20,7 +20,7 @@ class Application extends BaseApplication
      *
      * @see src/config.xml
      */
-    const VERSION = '2.0.0-beta.44';
+    const VERSION = '2.0.x-dev';
 
 // @codingStandardsIgnoreStart
     /**
